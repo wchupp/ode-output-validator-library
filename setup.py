93caa611a84a7793b5da56839fb883d13f29bf7b
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-04-02 15:40:00 -0400

    Fix package issues

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.01",
     author_email="fake@email.com",
     description="ODE Data Validation Library",
-    packages=['odevalidator']
+    packages=['odevalidator', 'result', 'sequential']
 )
