bd6d6d1b7be28aff96a8497f317979447a77fb80
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-04-02 15:50:06 -0400

    Fix packaging issues

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name="odevalidator",
-    version="0.01",
+    version="0.1",
     author_email="fake@email.com",
     description="ODE Data Validation Library",
-    packages=['odevalidator']
+    py_modules = ['odevalidator', 'result', 'sequential'],
+    packages=find_packages()
 )
